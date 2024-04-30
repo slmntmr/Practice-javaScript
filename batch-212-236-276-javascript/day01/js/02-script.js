bdc7f67aba779d10f230055696b6955115d56c51
Süleyman TİMUR <slmn1905tmr@gmail.com>
2024-04-30 12:41:24 +0300

    Practice javaScript

---
+++ batch-212-236-276-javascript/day01/js/02-script.js
@@ -0,0 +1,26 @@
+console.log("hello  external script");
+
+
+//consol turleri
+
+//1. console.log
+
+console.log("hello js");
+
+//2.console.warn
+
+console.warn("bu bir uyarıdır");
+
+//3.console.error
+
+console.error("bu bir hata");
+
+//4.console.info
+
+console.info("bu bilgi");
+
+
+
+
+
+
